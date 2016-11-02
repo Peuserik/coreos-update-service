8e01d3fce54c5c6af88c6803be27c21103ee7f2f
pegerto <pegerto@Pegerto-Mac.local>
2016-11-02 10:00:23 +0000

    [#7] Include additional storage test

--- coreos/storage_test.go
+++ coreos/storage_test.go
@@ -21,6 +21,57 @@ func (suite storageTestSuite) TestLocalDBCreatesFile() {
 	assert.True(suite.T(), info.IsDir(), "Local storage creates a directory")
 }
 
+// Test nodes
+func (suite storageTestSuite) TestRegisterNode() {
+	const machineId = "aaaa-bbbb-ccc-dddd"
+	storage := NewLocalDB(path)
+	storage.RegisterNode(machineId, "ExTrack", "omd", "123")
+
+	nodes := storage.ListNodes()
+
+	assert.NotNil(suite.T(), nodes[machineId], "Server is listed")
+}
+
+// Test a version version update
+func (suite storageTestSuite) TestUpdateVersion() {
+	const versionId = "1.2.3"
+	storage := NewLocalDB(path)
+	version := CoreOSVersion{
+		VersionId: versionId,
+		URL: "http://test_url.com",
+		Name: "update.gz",
+		Hash: "abcdef",
+		Signature: "abcdef",
+		Size: 2000,
+	}
+	storage.UpdateVersion(version)
+
+	version2 := storage.GetVersion(versionId)
+	assert.NotNil(suite.T(), version2)
+	assert.Equal(suite.T(), version, version2, "Storage version and reload aren't equal")
+}
+
+// Test update tracks
+func (suite storageTestSuite) TestTrackUpdate() {
+	storage := NewLocalDB(path)
+	tracks := map[string]string {
+		"TestTrack": "1.2.3",
+	}
+	storage.UpdateTracks(tracks)
+
+	tracks2 := storage.GetTracks()
+	assert.NotNil(suite.T(), tracks2)
+	assert.Equal(suite.T(), tracks, tracks2, "Tracks storage and reload aren't equal")
+}
+
+
+func (suite storageTestSuite) TestLogEvent() {
+	storage := NewLocalDB(path)
+	storage.LogEvent("aaaa-bbbb-ccc-dddd", "", "", "")
+	assert.True(suite.T(), true)
+}
+
+
 // Remove level db directory after each test execution
 func (suite storageTestSuite) TearDownTest() ()  {
 	os.RemoveAll(path)
